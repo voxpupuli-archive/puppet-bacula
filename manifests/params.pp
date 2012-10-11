ae91a12aa30cc8d481177b36730f165b935418fb
Russell Harrison <rharrison@fedoraproject.org>
2012-10-11 15:25:15 -0400

    Alphabetize params for easier management

--- manifests/params.pp
+++ manifests/params.pp
@@ -30,12 +30,6 @@
 #
 class bacula::params {
 
-# Define default values
-  $director_server_default = "bacula.${::domain}"
-  $storage_server_default = "bacula.${::domain}"
-  $mail_to_default= "root@${::fqdn}"
-
-# Define system package names
   $bat_console_package = $::operatingsystem ? {
     /(Debian|Ubuntu)/ => 'bacula-console-qt',
     default           => 'bacula-console-bat',
@@ -43,14 +37,16 @@ class bacula::params {
   $console_package = 'bacula-console'
   $director_mysql_package = 'bacula-director-mysql'
   $director_postgresql_package = 'bacula-director-postgresql'
-  $director_sqlite_package = 'bacula-director-sqlite'
-  $storage_mysql_package = 'bacula-storage-mysql'
-  $storage_postgresql_package = 'bacula-storage-postgresql'
-  $storage_sqlite_package = 'bacula-storage-sqlite'
-
-# Define system services
+  $director_server_default = "bacula.${::domain}"
   $director_service = $::operatingsystem ? {
     /(Debian|Ubuntu)/ => 'bacula-director',
     default           => 'bacula-dir',
   }
+  $director_sqlite_package = 'bacula-director-sqlite'
+  $mail_to_default= "root@${::fqdn}"
+  $storage_mysql_package = 'bacula-storage-mysql'
+  $storage_postgresql_package = 'bacula-storage-postgresql'
+  $storage_server_default = "bacula.${::domain}"
+  $storage_sqlite_package = 'bacula-storage-sqlite'
+
 }
