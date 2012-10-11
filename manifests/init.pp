beb43a993c5eb24168b6a822c332ef4da2296355
Russell Harrison <rharrison@fedoraproject.org>
2012-10-11 14:58:23 -0400

    Proper application of the ASL 2.0

--- manifests/init.pp
+++ manifests/init.pp
@@ -85,6 +85,25 @@
 #    storage_server    => 'bacula.domain.com',
 #    clients           => $clients,
 #  }
+#
+# === Copyright
+#
+# Copyright 2012 Russell Harrison
+#
+# === License
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#    http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
 class bacula (
   $console_password   = '',
   $console_template   = undef,
