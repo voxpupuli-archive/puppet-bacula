9fcd9738947ab6ea8900b88196ca4a4153ef5e1d
Russell Harrison <rharrison@fedoraproject.org>
2012-10-11 14:26:15 -0400

    Add director_password param to client::config define

--- manifests/client/config.pp
+++ manifests/client/config.pp
@@ -7,22 +7,45 @@
 # [*client_schedule*]
 #   The schedule for backups to be performed.
 #
+# [*director_password*]
+#   The director's password the client is connecting to.
+#
 # [*fileset*]
 #   The file set used by the client for backups
 #
 # === Examples
 #
 #   bacula::client::config { 'client1.example.com' :
-#     fileset         => 'Basic:noHome',
-#     client_schedule => 'WeeklyCycle',
+#     client_schedule   => 'WeeklyCycle',
+#     director_password => 'directorpassword',
+#     fileset           => 'Basic:noHome',
 #   }
 #
 define bacula::client::config (
-  $client_schedule  = 'WeeklyCycle',
-  $fileset          = 'Basic:noHome'
+  $client_schedule   = 'WeeklyCycle',
+  $director_password = undef,
+  $director_server   = undef,
+  $fileset           = 'Basic:noHome'
 ) {
+  case $director_password {
+    undef   : {
+      case $bacula::director::director_password {
+        undef, '' : {
+          fail('You must provide a value for director_password or create the',
+            'configs by passing the clients hash to bacula class on the director server'
+          )
+        }
+        default   : {
+          $director_password_real = $bacula::director::director_password
+        }
+      }
+    }
+    default : {
+      $director_password_real = $director_password
+    }
+  }
 
-  if ! is_domain_name($name) {
+  if !is_domain_name($name) {
     fail "Name for client ${name} must be a fully qualified domain name"
   }
 
